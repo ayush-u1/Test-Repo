40112ae46a70e3de228f5a4b51a7ca878fce94b5
ayush-u1 <Ayush.Upadhyay@TriconInfotech.com>
2023-02-09 11:53:39 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+/*decorator ---- conponent decorator is basically a fubnction that attaches to class just below it*/
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  //metadata
+  selector: 'app-root',//selector is custom html tag
+  templateUrl: './app.component.html', //templateurl points to the html flies that represent view for this component
   styleUrls: ['./app.component.css']
 })
+//class Appcompnt 
 export class AppComponent {
-  title = 'helloworld';
+  title = 'Ayush';
 }
